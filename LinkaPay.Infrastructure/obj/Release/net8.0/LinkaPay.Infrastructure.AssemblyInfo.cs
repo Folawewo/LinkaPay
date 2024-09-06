fe146a09d7824485c10ec41cac92ec97b2882ba3
Folawewo <famwallexie@gmail.com>
2024-09-06 16:54:04 +0100

    tests

--- LinkaPay.Infrastructure/obj/Release/net8.0/LinkaPay.Infrastructure.AssemblyInfo.cs
+++ LinkaPay.Infrastructure/obj/Release/net8.0/LinkaPay.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LinkaPay.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8082659d4104188c74630d0eca09c30b4de0d5e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3cd1681ced806e31fde25046ac910f4c2a67b26")]
 [assembly: System.Reflection.AssemblyProductAttribute("LinkaPay.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LinkaPay.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
